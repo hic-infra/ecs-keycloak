662d33b47c214504a74746f151e58548481bfdc0
Simon Li <spli@dundee.ac.uk>
2023-01-31 10:35:14 +0000

    Update keycloak-image var for planned 1.0.0-beta.2

--- ecs-cluster/variables.tf
+++ ecs-cluster/variables.tf
@@ -12,7 +12,7 @@ variable "region" {
 
 variable "keycloak-image" {
   type        = string
-  default     = "ghcr.io/hic-infra/ecs-keycloak:1.0.0-beta.1"
+  default     = "ghcr.io/hic-infra/ecs-keycloak:1.0.0-beta.2"
   description = "Keycloak image including registry"
 }
 
