d381fa5a9b47edbd42a19945f95e296124dc2d22
Simon Li <spli@dundee.ac.uk>
2024-03-06 13:08:13 +0000

    Allow KC_DB to be overridden at runtime

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -6,4 +6,10 @@ set -eu
 cd /opt/keycloak
 keytool -genkeypair -storepass password -storetype PKCS12 -keyalg RSA -keysize 2048 -dname "CN=$KC_HOSTNAME" -alias server -ext "SAN:c=DNS:$KC_HOSTNAME,DNS:localhost,IP:127.0.0.1" -keystore conf/server.keystore
 
+# If KC_DB has been overridden then need to rebuild the config
+if [ "$KC_DB" != postgres ]; then
+  echo "KC_DB has been overridden to $KC_DB, rebuilding"
+  /opt/keycloak/bin/kc.sh build
+fi
+
 exec /opt/keycloak/bin/kc.sh "$@"
