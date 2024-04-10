3fe95191fab4eaee2e0bb9f7e6ee69fb6138201d
Simon Li <spli@dundee.ac.uk>
2024-04-10 17:16:48 +0100

    Add `terraform {backend s3}` back
    Partially reverts commit dc8987cae95e46384c69e62aedd4ac6de0498fe7.

--- ecs-cluster/terraform.tf
+++ ecs-cluster/terraform.tf
@@ -1,3 +1,10 @@
+# The Terraform state should be stored in S3, but you must configure the S3 bucket/prefix separately.
+# See example.s3.tfbackend
+terraform {
+  backend "s3" {
+  }
+}
+
 provider "aws" {
   region = var.region
 
