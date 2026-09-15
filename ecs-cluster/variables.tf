e3d0df0e2ba1cbf6aca35f051968ee169e61772a
Aaron Jackson <ajackson002@dundee.ac.uk>
2026-09-15 10:08:05 +0100

    Tag container build (2.4.0)

--- ecs-cluster/variables.tf
+++ ecs-cluster/variables.tf
@@ -12,7 +12,7 @@ variable "region" {
 
 variable "keycloak-image" {
   type        = string
-  default     = "ghcr.io/hic-infra/ecs-keycloak:2.2.1"
+  default     = "ghcr.io/hic-infra/ecs-keycloak:2.4.0"
   description = "Keycloak image including registry"
 }
 
